cc70b7b9ccb8a506b9c4968c50e643a995642c0e
Felix Titov <titow.feliks@yandex.ru>
2021-10-25 12:24:28 +0300

    Changed mustache on freemarker

--- src/main/java/com/example/socialnetwork/controllers/MainController.java
+++ src/main/java/com/example/socialnetwork/controllers/MainController.java
@@ -7,6 +7,7 @@ import com.example.socialnetwork.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,9 +26,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -46,17 +53,5 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-
-        model.put("messages", messages);
-        return "main";
-    }
 
 }
