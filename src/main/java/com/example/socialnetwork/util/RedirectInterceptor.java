b7451d3567343035a7962a6c54fc9b2a37b509b2
Felix Titov <titow.feliks@yandex.ru>
2021-11-07 23:45:17 +0300

    Fixed url for Turbolinks

--- src/main/java/com/example/socialnetwork/util/RedirectInterceptor.java
+++ src/main/java/com/example/socialnetwork/util/RedirectInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.socialnetwork.util;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -12,7 +13,12 @@ public class RedirectInterceptor extends HandlerInterceptorAdapter {
         //super.postHandle(request, response, handler, modelAndView);
         if(modelAndView != null) {
             String args = request.getQueryString() != null ? request.getQueryString() : "";
-            String url = request.getRequestURI().toString() + "?" +args;
+            String url;
+            if (!StringUtils.isEmpty(args)) {
+                url = request.getRequestURI().toString() + "?" + args;
+            } else {
+                url = request.getRequestURI().toString();
+            }
             response.setHeader("Turbolinks-Location", url );
         }
     }
